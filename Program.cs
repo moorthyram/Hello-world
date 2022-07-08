02a04ce406f593fa03b8e4a05b7b0376689abc64
Ramamoorthy <ramamoorthy300798@gmail.com>
2022-07-08 08:57:36 +0530

    hello world

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.ReadLine();
+            Console.WriteLine("I am Ram");
+        }
+    }
+}
